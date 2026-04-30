61896116adfb33bb26d80de5cc63f06ef76cf808
jackmarios10 <jacobkarakog@gmail.com>
2026-04-30 15:34:55 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,36 @@
 'use strict';
 
 
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+const target = +counter.getAttribute("data-target");
+
+let count = 0;
+
+const updateCounter = () => {
+
+const increment = target / 60;
+
+count += increment;
+
+if(count < target){
+
+counter.innerText = Math.ceil(count);
+
+requestAnimationFrame(updateCounter);
+
+}else{
+
+counter.innerText = target;
+
+}
+
+};
+
+updateCounter();
+
 
 /**
  * PRELOADER
